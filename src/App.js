905fa01e5ab07c16c6cbf25b22d6f83beb61c08d
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-04-26 17:49:49 -0700

    added the navigation, left logo, and the style

--- src/App.js
+++ src/App.js
@@ -1,54 +1,91 @@
+import { render } from "react-dom";
+import React, { useState, useCallback } from "react";
+import { useTransition, animated } from "react-spring";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Projects from "./Components/Projects/Projects";
+import Contact from "./Components/Contact/Contact";
+import NavigationBar from "./Components/NavigationBar/NavigationBar";
 
-import { render } from 'react-dom'
-import React, { useState, useCallback } from 'react'
-import { useTransition, animated } from 'react-spring'
-import Home from "./Components/Home/Home"
-import About from "./Components/About/About"
-import Projects from "./Components/Projects/Projects"
-import Contact from "./Components/Contact/Contact"
-
-import './index.css'
+import "./index.css";
 
 const pages = [
-({ style }) => <animated.div style={{ ...style, background: '#EAECEF' }}><Home /></animated.div>,
-  ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}><Projects /></animated.div>,
-  ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}><About /></animated.div>,
-  ({ style }) => <animated.div style={{ ...style, background: 'tomato' }}><Contact /></animated.div>,
-]
+  ({ style }) => (
+    <animated.div style={{ ...style, background: "#EAECEF" }}>
+      <Home />
+    </animated.div>
+  ),
+  ({ style }) => (
+    <animated.div style={{ ...style, background: "lightblue" }}>
+      <Projects />
+    </animated.div>
+  ),
+  ({ style }) => (
+    <animated.div style={{ ...style, background: "lightgreen" }}>
+      <About />
+    </animated.div>
+  ),
+  ({ style }) => (
+    <animated.div style={{ ...style, background: "tomato" }}>
+      <Contact />
+    </animated.div>
+  ),
+];
 
 export default function App() {
+  const [index, set] = useState(0);
+  const onClickRight = useCallback(() => set((state) => (state + 1) % 4), []);
+  const onClickLeft = useCallback(
+    () => set((state) => (state <= 0 ? "" : state - 1) % 4),
+    []
+  );
 
-  const [index, set] = useState(0)
-  const onClickRight = useCallback(() => set(state => (state + 1) % 4), [])
-  const onClickLeft = useCallback(() => set(state => ( state <= 0 ?"" : state - 1) % 4), [])
-  
-  const transitions = useTransition(index, p => p, {
-    from: { opacity: 0, transform: 'translate3d(0,100%,0)' },
-    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
-  })
-  
+  const transitions = useTransition(index, (p) => p, {
+    from: { opacity: 0, transform: "translate3d(0,100%,0)" },
+    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+  });
+
   return (
+    <section>
+      <nav className="navigation-bar">
+        <p>Home</p>
+        <p>Projects</p>
+        <p>About</p>
+        <p>Contact</p>
+      </nav>
 
-    <section id="main-container" className="simple-trans-main ">
-      {transitions.map(({ item, props, key }) => {
-        const Page = pages[item]
-        return <Page key={key} style={props} />
-      })}
+      <div id="left-logo">
+        <p>
+          Bruno <br />
+          <span>DaSilva</span>
+        </p>
+      </div>
 
-      <article className="btn-holder-right">
-        <button className="btn-move-right display-right" onClick={onClickRight}>
-          Move right
-        </button>
-      </article>
 
-      <article className="btn-holder-left">
-        <button className="btn-move-left display-left" onClick={onClickLeft}>
-          Move left
-        </button>
-      </article>
+      <main id="main-container" className="simple-trans-main ">
+        {transitions.map(({ item, props, key }) => {
+          const Page = pages[item];
+          return <Page key={key} style={props} />;
+        })}
+
+        <article className="btn-holder-right">
+          <button
+            className="btn-move-right display-right"
+            onClick={onClickRight}
+          >
+            Move right
+          </button>
+        </article>
+
+        <article className="btn-holder-left">
+          <button className="btn-move-left display-left" onClick={onClickLeft}>
+            Move left
+          </button>
+        </article>
+      </main>
     </section>
-  )
+  );
 }
 
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById("root"));
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./home-CSS/home-style.css"
 import logo from "./images/blogo-gray.png"
 
+
 class Home extends Component {
     render() {
         return (
