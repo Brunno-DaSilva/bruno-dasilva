46a59ff9c373c2510ea26b24e7404c818957e93d
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-07-14 12:20:02 -0500

    Added react dragGesture to logo

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
+import { useDrag } from "react-use-gesture";
 
 import "./home-CSS/home-style.css";
 import "../../index.css";
 import logo from "./images/blogo-gray.png";
 
-const Home = () => {
+function Home() {
+  const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }));
+  const bind = useDrag(({ down, movement }) => {
+    set({ xy: down ? movement : [0, 0] });
+  });
+
   return (
-    <section className="section-container animated delay-1s fadeInUp">
-      <img src={logo} />
-      <p className="home-logo-p">Front-end Development</p>
-    </section>
+    <>
+      <section className="section-container animated delay-1s fadeInUp">
+        <animated.div
+          id="divID"
+          {...bind()}
+          style={{
+            transform: xy.interpolate(
+              (x, y) => `translate3d(${x}px, ${y}px, 0)`
+            ),
+          }}
+        >
+          <img src={logo} />
+          <p className="home-logo-p">Front-end Development</p>
+        </animated.div>
+      </section>
+    </>
   );
-};
+}
 
 export default Home;
