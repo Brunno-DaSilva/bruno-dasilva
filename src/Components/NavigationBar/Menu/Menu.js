8a334c240235aa1d0b76e4776608dad75abe957b
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-07-23 12:18:53 -0500

    Added active link style on nav

--- src/Components/NavigationBar/Menu/Menu.js
+++ src/Components/NavigationBar/Menu/Menu.js
@@ -2,20 +2,35 @@ import React from "react";
 import { Link } from "@reach/router";
 import SideDrawerButton from "../SideDrawer/SideDrawerButton";
 
+const NavLink = (props) => (
+  <Link
+    {...props}
+    getProps={({ isCurrent }) => {
+      // the object returned here is passed to the
+      // anchor element's props
+      return {
+        style: {
+          color: isCurrent ? "#26b2e0" : "#9e9d9d",
+        },
+      };
+    }}
+  />
+);
+
 const Menu = (props) => {
   return (
     <nav className="navigation-bar ">
       <p className="navigation-bar__item">
-        <Link to="/">Home</Link>
+        <NavLink to="/">Home</NavLink>
       </p>
       <p className="navigation-bar__item">
-        <Link to="about">About</Link>
+        <NavLink to="about">About</NavLink>
       </p>
       <p className="navigation-bar__item">
-        <Link to="projects">Projects</Link>
+        <NavLink to="projects">Projects</NavLink>
       </p>
       <p className="navigation-bar__item">
-        <Link to="contact">Contact</Link>
+        <NavLink to="contact">Contact</NavLink>
       </p>
       <p className="side-draw-button">
         <SideDrawerButton click={props.drawerClickHandler} />
